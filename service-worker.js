b04b3df05d1d47846d220fb2cb26e14ae2cf0a87
Nicholas Ewasiuk <ewasiuk.nick@gmail.com>
2021-09-12 20:19:17 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.6f8dcfe334de12fd7b9978bd4b8323cb.js"
+  "./precache-manifest.82fba24a765053aaa93eff5aa08280e7.js"
 );
 
 self.addEventListener('message', (event) => {
